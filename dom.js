2fa39d459b20154b9967079361d87b5782702179
Akshxar <shakya.ani47@gmail.com>
2023-09-08 12:24:46 +0530

    DOM ITEM BOLD AND GREEN

--- dom.js
+++ dom.js
@@ -22,11 +22,33 @@ document.all[10].textContent="Making lot of list items"
 
 // console.log(document.getElementById("header-title"))
 
-var headertitle=document.getElementById("header-title")
-headertitle.textContent="FOOD LIST ITEMS"
-headertitle.style.borderBlock="solid"
+var headertitle=document.getElementById("header-title");
+headertitle.textContent="FOOD LIST ITEMS";
+headertitle.style.borderBlock="solid";
 
 // changing main header borderBottom
 
-var mainheader=document.getElementById("main-header")
-mainheader.style.borderBottom="solid 3px #000"
+var mainheader=document.getElementById("main-header");
+mainheader.style.borderBottom="solid 3px #000";
+
+// accessing elements by class and changing the colors and styles 
+
+var items=document.getElementsByClassName("list-group-item");
+//console.log(items);
+//console.log(items[0]);
+
+items[0].textContent="ENTER ITEM 1 ";
+items[0].style.fontWeight = 'bold';
+items[0].style.backgroundColor='grey';
+
+// loop is used to make all the items background color to green and bold
+// according to sharpener question
+
+for (var i=0; i<items.length; i++)
+
+{
+
+    items[i].style.fontWeight="bold"
+    items[i].style.backgroundColor="green"
+ 
+}
