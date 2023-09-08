7843599cbe6821766a1888eb0bdcb75d198cfa35
Akshxar <shakya.ani47@gmail.com>
2023-09-08 11:07:59 +0530

    listitemformready

---
+++ dom.js
@@ -0,0 +1,19 @@
+console.dir(document);
+// EXAMINE THE DOCUMENT OBJECT 
+
+//console.log(document.URL);
+//console.log(document.title);
+
+document.title="LEARNING THE DOM ";
+
+//console.log(document.doctype);
+//console.log(document.body);
+//console.log(document.all);
+
+//console.log(document.all[10]);
+//console.log(document.forms);
+//console.log(document.forms[0]);
+
+//console.log(document.all[10]);
+
+document.all[10].textContent="Making lot of list items"
